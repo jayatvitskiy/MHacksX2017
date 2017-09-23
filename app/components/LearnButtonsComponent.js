77680c502f3b8f1ddc4947d9f269786dd58aea4a
Jacob Yatvitskiy <yatvij@umich.edu>
2017-09-23 05:55:54 -0400

    changes

--- app/components/LearnButtonsComponent.js
+++ app/components/LearnButtonsComponent.js
@@ -1,51 +1,57 @@
+function submitDisable(char)
+                {submitImage(char);disableAll();}
 const React = require('react');
 
 export default class LearnButtonComponent extends React.Component {
+    
     render() {
         return (
             <div>
-                <div className="row">
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="A" href = "/learnScript/A">A</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="B" href = "/learnScript/B">B</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="C" href = "/learnScript/C">C</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="D" href = "/learnScript/D">D</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="E" href = "/learnScript/E">E</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="F" href = "/learnScript/F">F</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="G" href = "/learnScript/G">G</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="H" href = "/learnScript/H">H</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="I" href = "/learnScript/I">I</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="J" href = "/learnScript/J">J</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="K" href = "/learnScript/K">K</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="L" href = "/learnScript/L">L</a></div>
-                </div>
-                <div className="row">
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="M" href = "/learnScript/M">M</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="N" href = "/learnScript/N">N</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="O" href = "/learnScript/O">O</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="P" href = "/learnScript/P">P</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="Q" href = "/learnScript/Q">Q</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="R" href = "/learnScript/R">R</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="S" href = "/learnScript/S">S</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="T" href = "/learnScript/T">T</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="U" href = "/learnScript/U">U</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="V" href = "/learnScript/V">V</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="W" href = "/learnScript/W">W</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="X" href = "/learnScript/X">X</a></div>
-                </div>
-                <div className="row">
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="Y" href = "/learnScript/Y">Y</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn pink accent-3" id="Z" href = "/learnScript/Z">Z</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn indigo" id="0" href = "/learnScript/0">0</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn indigo" id="1" href = "/learnScript/1">1</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn indigo" id="2" href = "/learnScript/2">2</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn indigo" id="3" href = "/learnScript/3">3</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn indigo" id="4" href = "/learnScript/4">4</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn indigo" id="5" href = "/learnScript/5">5</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn indigo" id="6" href = "/learnScript/6">6</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn indigo" id="7" href = "/learnScript/7">7</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn indigo" id="8" href = "/learnScript/8">8</a></div>
-                    <div className="col s1"><a className="waves-effect waves-light btn indigo" id="9" href = "/learnScript/9">9</a></div>
-                </div>
+                <div className="row"> 
+                
+                        <div className="col s1" onClick="submitDisable(A);"><a id = "A" className = "waves-effect waves-light btn pink accent-3">A</a></div>
+                        <div className="col s1" onClick="submitDisable(B);"><a id = "B" className = "waves-effect waves-light btn pink accent-3">B</a></div>
+                        <div className="col s1" onClick="submitDisable(C);"><a id = "C" className = "waves-effect waves-light btn pink accent-3">C</a></div>
+                        <div className="col s1" onClick="submitDisable(D);"><a id = "D" className = "waves-effect waves-light btn pink accent-3">D</a></div>
+                        <div className="col s1" onClick="submitDisable(E);"><a id = "E" className = "waves-effect waves-light btn pink accent-3">E</a></div>
+                        <div className="col s1" onClick="submitDisable(F);"><a id = "F" className = "waves-effect waves-light btn pink accent-3">F</a></div>
+                        <div className="col s1" onClick="submitDisable(G);"><a id = "G" className = "waves-effect waves-light btn pink accent-3">G</a></div>
+                        <div className="col s1" onClick="submitDisable(H);"><a id = "H" className = "waves-effect waves-light btn pink accent-3">H</a></div>
+                        <div className="col s1" onClick="submitDisable(I);"><a id = "I" className = "waves-effect waves-light btn pink accent-3">I</a></div>
+                        <div className="col s1" onClick="submitDisable(J);"><a id = "J" className = "waves-effect waves-light btn pink accent-3">J</a></div>
+                        <div className="col s1" onClick="submitDisable(K);"><a id = "K" className = "waves-effect waves-light btn pink accent-3">K</a></div>
+                        <div className="col s1" onClick="submitDisable(L);"><a id = "L" className = "waves-effect waves-light btn pink accent-3">L</a></div>
+                    </div>
+                    <div className="row"> 
+                        <div className="col s1" onClick="submitDisable(M);"><a id = "M" className = "waves-effect waves-light btn pink accent-3">M</a></div>
+                        <div className="col s1" onClick="submitDisable(N);"><a id = "N" className = "waves-effect waves-light btn pink accent-3">N</a></div>
+                        <div className="col s1" onClick="submitDisable(O);"><a id = "O" className = "waves-effect waves-light btn pink accent-3">O</a></div>
+                        <div className="col s1" onClick="submitDisable(P);"><a id = "P" className = "waves-effect waves-light btn pink accent-3">P</a></div>
+                        <div className="col s1" onClick="submitDisable(Q);"><a id = "Q" className = "waves-effect waves-light btn pink accent-3">Q</a></div>
+                        <div className="col s1" onClick="submitDisable(R);"><a id = "R" className = "waves-effect waves-light btn pink accent-3">R</a></div>
+                        <div className="col s1" onClick="submitDisable(S);"><a id = "S" className = "waves-effect waves-light btn pink accent-3">S</a></div>
+                        <div className="col s1" onClick="submitDisable(T);"><a id = "T" className = "waves-effect waves-light btn pink accent-3">T</a></div>
+                        <div className="col s1" onClick="submitDisable(U);"><a id = "U" className = "waves-effect waves-light btn pink accent-3">U</a></div>
+                        <div className="col s1" onClick="submitDisable(V);"><a id = "V" className = "waves-effect waves-light btn pink accent-3">V</a></div>
+                        <div className="col s1" onClick="submitDisable(W);"><a id = "W" className = "waves-effect waves-light btn pink accent-3">W</a></div>
+                        <div className="col s1" onClick="submitDisable(X);"><a id = "X" className = "waves-effect waves-light btn pink accent-3">X</a></div>
+                    </div>
+                    <div className="row">
+							<div className="col s1" onClick="submitDisable(Y);"><a id = "Y" className = "waves-effect waves-light btn pink accent-3">Y</a></div>
+							<div className="col s1" onClick="submitDisable(Z);"><a id = "Z" className = "waves-effect waves-light btn pink accent-3">Z</a></div>
+							<div className="col s1" onClick="submitDisable(0);"><a id = "0" className = "waves-effect waves-light btn indigo">0</a></div>
+							<div className="col s1" onClick="submitDisable(1);"><a id = "1" className = "waves-effect waves-light btn indigo">1</a></div>
+							<div className="col s1" onClick="submitDisable(2);"><a id = "2" className = "waves-effect waves-light btn indigo">2</a></div>
+							<div className="col s1" onClick="submitDisable(3);"><a id = "3" className = "waves-effect waves-light btn indigo">3</a></div>
+							<div className="col s1" onClick="submitDisable(4);"><a id = "4" className = "waves-effect waves-light btn indigo">4</a></div>
+							<div className="col s1" onClick="submitDisable(5);"><a id = "5" className = "waves-effect waves-light btn indigo">5</a></div>
+							<div className="col s1" onClick="submitDisable(6);"><a id = "6" className = "waves-effect waves-light btn indigo">6</a></div>
+							<div className="col s1" onClick="submitDisable(7);"><a id = "7" className = "waves-effect waves-light btn indigo">7</a></div>
+							<div className="col s1" onClick="submitDisable(8);"><a id = "8" className = "waves-effect waves-light btn indigo">8</a></div>
+							<div className="col s1" onClick="submitDisable(9);"><a id = "9" className = "waves-effect waves-light btn indigo">9</a></div>
+				    </div>
+
+
             </div>
         );
     };
--- app/components/PracticeComponent.js
+++ app/components/PracticeComponent.js
@@ -3,27 +3,49 @@ const React = require('react');
 export default class PracticeComponent extends React.Component {
     render() {
         return (
+            
             <div id="practice" className="col s12">
-                <div className="row">
-                    <form method="POST" action="/runScript" className="learn">
-                        <br/><br/><br/><br/>
-                        <div>You will need a webcam feed in order for the computer to</div>
-                        <div>recognize your gestures so that you can begin learning ASL.</div>
-                        <br/>
-                        <div>Please click the button below to open the webcam feed.</div>
-                        <br/>
-                        <div>It is recommended that you resize the feed</div>
-                        <div>window to fit within the contours of this box.</div>
-                        <br/>
-                        <div>It is also recommended that your hand take up as much</div>
-                        <div>of the white outlined box in the webcam feed as possible.</div>
-                        <br/><br/>
-                        <button className="btn waves-effect waves-light indigo" type="submit" name="submit">Practice
-                            <i className="material-icons right">send</i>
-                        </button>
-                    </form>
+            <div className="chip chip1" id="chip6">one</div>
+            <div className="chip chip2" id="chip7">two</div>
+            <div className="chip chip3" id="chip8">three</div>
+            <div className="chip chip4" id="chip9">four</div>
+            <div className="chip chip5" id="chip10">five</div>
+            
+                <div className="row"> 
+                            <div id = "letterdiv" ></div>
+                            <div id = "response2"  ></div>
+                            <div  className = "mybox" ></div>
+                    <div className="camera">
+                    <video id = "forVid2"  autoPlay="true" height="400px" width="600px" ></video>
+                    </div>
+                    <canvas id="canvas"></canvas>
+
                 </div>
+            
+                <div className = "row">
+                            <div className = "col s3 push-s4">
+                                <button className="btn waves-effect waves-light indigo" id = "practicebtn"  onClick="startCount();this.disabled = true">Start
+                                </button> 
+                            </div>
+                            <div className = "col s3 push-s2">
+                                <button className="btn waves-effect waves-light pink accent-3" id = "endpracticebtn" onClick="stopCount();enablePractice();">End
+                                </button> 
+                            </div>
+                        </div>
+                        <div className="row">
+                        </div>
+            
+
+            
+            
+            <div className="chip pink accent-3 white-text" >get ready in </div>
+            
+
+					
+
+                
             </div>
         );
     };
 }
+

cb22f1f7b7d14eace4983f952f0774e59b591400
Jacob A Yatvitskiy <yatvij@dcllcp04.dc.umich.edu>
2017-09-24 15:50:13 -0400

    Restored ML

--- MachineLearning/code/comparetest_ml.py
+++ MachineLearning/code/comparetest_ml.py
@@ -20,23 +20,19 @@ model_name="logistic"
 user_sign_dir = "../data/images/test"
 for filename in os.listdir(user_sign_dir):
     if filename.endswith(".jpg"):
-        frame = cv2.imread(filename)
+        frame = cv2.imread(user_sign_dir+'/'+filename)
         try:
             frame = apply_image_transformation(frame)
             frame_flattened = frame.flatten()
             classifier_model = joblib.load(model_serialized_path)
             predicted_labels = classifier_model.predict(frame_flattened)
             predicted_label = predicted_labels[0]
-            print("Predicted label = {}".format(predicted_labels))
 
             if image_label != predicted_label:
-                print("Incorrect prediction!!")
-                cv2.waitKey(5000)
+                return false
         except Exception:
-            exception_traceback = traceback.format_exc()
-            print("Error while applying image transformation on image path '{}' with the following exception trace:\n{}".format(
-                filename, exception_traceback))
-            continue
+            return false
+	return true
 cv2.destroyAllWindows()
-print "The program completed successfully !!"
+
 
